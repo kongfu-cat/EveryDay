568ce003433c4ccecd8a08b9784880b336a6cf14
Leye Hu <huleye@outlook.com>
2020-09-15 01:06:37 +0800

    feat: add lotto random selector

--- meta.js
+++ meta.js
@@ -5,4 +5,10 @@ export default [
         date: 1598019537241,
         path: 'blog/2020/08/hello-world/index.html',
     },
+    {
+        title: `Lotto Random`,
+        description: `随机彩票选择，机选。`,
+        date: 1600103083991,
+        path: 'blog/2020/09/lotto/index.html',
+    },
 ];
